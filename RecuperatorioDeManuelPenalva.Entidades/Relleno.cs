8a5698c4876439a1f798658a2808d102aee957b5
manue <manue@LAPTOP-TKTP7A3B>
2022-12-27 20:14:28 -0300

    Agregar archivos de proyecto.

---
+++ RecuperatorioDeManuelPenalva.Entidades/Relleno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecuperatorioDeManuelPenalva.Entidades
+{
+    public enum Relleno
+    {
+        
+        Blanco = 1,
+        Negro,
+        Rojo,
+        Azul,
+        Verde
+    }
+}
+
